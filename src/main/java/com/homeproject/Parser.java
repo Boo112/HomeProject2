ddef6ab974dab99c42975a01069b790b517e6859
Ivan Butin <i.butin0@gmail.com>
2019-03-18 20:25:30 +0500

    new class ParserHelper

--- src/main/java/com/homeproject/Parser.java
+++ src/main/java/com/homeproject/Parser.java
@@ -12,6 +12,8 @@ public class Parser {
 
     GeneratData generatData=new GeneratData();
     PathToFiles fPath=new PathToFiles();
+    ParserHelper parserHelper=new ParserHelper();
+
     Random r = new Random();
 
     // Получаем пользователей из локальной базы
@@ -75,14 +77,14 @@ public class Parser {
         JsonElement dateOfBirth = getValue(dob, "date");
 
         String flat = String.valueOf(1 + r.nextInt(500));
-        String house = getHouseFromStreet(street.toString().replace("\"", ""));
+        String house = parserHelper.getHouseFromStreet(street.toString().replace("\"", ""));
         String inn = String.valueOf(generatData.getInn()); // ИНН формируем из локальной базы
         String street1 = street.toString().replace("\"", "");
-        String country = getCountry(nationality.toString().replace("\"", ""));
+        String country = parserHelper.getCountry(nationality.toString().replace("\"", ""));
 
         return new User(
                 getFormatedData(firstName), getFormatedData(lastName), "no patronymic",
-                getFormatedData(age), getFormatedData(gender), getFormatDate(getFormatedData(dateOfBirth)),
+                getFormatedData(age), getFormatedData(gender), parserHelper.getFormatDate(getFormatedData(dateOfBirth)),
                 getFormatedData(city), street1.replaceAll(house, ""),
                 country, flat, inn, getFormatedData(index), house, getFormatedData(state)
         );
@@ -99,100 +101,13 @@ public class Parser {
         return null;
     }
 
-    //Получаем номер дома из названия улицы
-    private String getHouseFromStreet(String street) {
-
-        return street.replaceAll("\\D+", "");
-
-    }
-
-    // Формируем дату полученную из Json в норм формат
-    private String getFormatDate(String dt) {
-
-        String day;
-        String year;
-        String month;
-        String date;
-
-        int k = dt.indexOf('T');
-
-        dt = dt.substring(0, k);
-        year = dt.substring(0, 4);
-        day = dt.substring(8, k);
-        month = dt.substring(5, 7);
-        date = day + "." + month + "." + year;
-
-        return date;
-    }
-
-    // Получаем страну из национальности
-    private String getCountry(String temp) {
-
-        String country;
-
-        //AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NO, NL, NZ, TR, US
-        switch (temp) {
-            case "BR":
-                country = "Brazil";
-                break;
-            case "US":
-                country = "United States";
-                break;
-            case "FR":
-                country = "France";
-                break;
-            case "CA":
-                country = "Canada";
-                break;
-            case "AU":
-                country = "Australia";
-                break;
-            case "CH":
-                country = "China";
-                break;
-            case "DE":
-                country = "Germany";
-                break;
-            case "DK":
-                country = "Denmark";
-                break;
-            case "ES":
-                country = "Spain";
-                break;
-            case "FI":
-                country = "Finland";
-                break;
-            case "GB":
-                country = "Great Britain";
-                break;
-            case "IE":
-                country = "Norway";
-                break;
-            case "NO":
-                country = "France";
-                break;
-            case "NL":
-                country = "France";
-                break;
-            case "IR":
-                country = "Turkey";
-                break;
-            case "NZ":
-                country = "New Zealand";
-                break;
-            case "TR":
-                country = "France";
-                break;
-            default:
-                country = "Unknown Country";
-                break;
-        }
-        return country;
-    }
-
     // Преобразование к нормальному виду данных из Json
     private String getFormatedData(JsonElement data) {
 
         return data.toString().replace("\"", "");
     }
+
+
+
+
 }
---
+++ src/main/java/com/homeproject/ParserHelper.java
@@ -0,0 +1,101 @@
+package com.homeproject;
+
+import com.google.gson.JsonElement;
+
+public class ParserHelper {
+
+    // Формируем дату для записи в эксель
+    public String getFormatDate(String dt) {
+
+        String day;
+        String year;
+        String month;
+        String date;
+
+        int k = dt.indexOf('T');
+
+        dt = dt.substring(0, k);
+        year = dt.substring(0, 4);
+        day = dt.substring(8, k);
+        month = dt.substring(5, 7);
+        date = day + "." + month + "." + year;
+
+        return date;
+    }
+
+    //Получаем номер дома из названия улицы
+    public String getHouseFromStreet(String street) {
+
+        return street.replaceAll("\\D+", "");
+
+    }
+
+    // Получаем страну из национальности
+    public String getCountry(String temp) {
+
+        String country;
+
+        //AU, BR, CA, CH, DE, DK, ES, FI, FR, GB, IE, IR, NO, NL, NZ, TR, US
+        switch (temp) {
+            case "BR":
+                country = "Brazil";
+                break;
+            case "US":
+                country = "United States";
+                break;
+            case "FR":
+                country = "France";
+                break;
+            case "CA":
+                country = "Canada";
+                break;
+            case "AU":
+                country = "Australia";
+                break;
+            case "CH":
+                country = "China";
+                break;
+            case "DE":
+                country = "Germany";
+                break;
+            case "DK":
+                country = "Denmark";
+                break;
+            case "ES":
+                country = "Spain";
+                break;
+            case "FI":
+                country = "Finland";
+                break;
+            case "GB":
+                country = "Great Britain";
+                break;
+            case "IE":
+                country = "Norway";
+                break;
+            case "NO":
+                country = "France";
+                break;
+            case "NL":
+                country = "France";
+                break;
+            case "IR":
+                country = "Turkey";
+                break;
+            case "NZ":
+                country = "New Zealand";
+                break;
+            case "TR":
+                country = "France";
+                break;
+            default:
+                country = "Unknown Country";
+                break;
+        }
+        return country;
+    }
+
+
+
+
+}
